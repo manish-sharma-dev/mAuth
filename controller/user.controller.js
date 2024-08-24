b46cffd61db003d56ceaba0c4077ab8a7adf17e0
manish sharma dev <manishvsharma1@gmail.com>
2024-08-24 06:07:13 -1200

    User is moved to login page created by google

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -3,41 +3,49 @@ import { Apiresponse } from "../utils/Apiresponse.util.js"
 import { google } from "googleapis";
 import crypto from 'crypto'
 
-const AuthorisefromGoogleusingOauth = async() => {
+const AuthorisefromGoogleusingOauth = async(req,res) => {
     
-    // oAuth Client info 
-    const oauth2Client = new google.auth.OAuth2(
-        process.env.GOOGLE_OAUTH_CLIENT_ID,
-        process.env.GOOGLE_OAUTH_CLIENT_SECRET,
-        process.env.REDIRECT_URI
-    );
-
-    console.log('oauthclient Connection successfull')
-
-    const scopes = [
-        'https://www.googleapis.com/auth/drive.metadata.readonly'
-    ];
-
-    console.log('Scope set done')
-
-    const state = crypto.randomBytes(32).toString('hex');
-
-    // Generate a url that asks permissions for the Drive activity scope
-
-    const authorizationUrl = oauth2Client.generateAuthUrl({
-    // 'online' (default) or 'offline' (gets refresh_token)
-    access_type: 'offline',
-    scope: scopes,
-    include_granted_scopes: true,
-    // Include the state parameter to reduce the risk of CSRF attacks.
-    state: state
-   });
-
-   if(!authorizationUrl){
-    throw new Apierror(500,'Failed to Get AUthorization Url')
+   try {
+     // oAuth Client info 
+     const oauth2Client = new google.auth.OAuth2(
+         process.env.GOOGLE_OAUTH_CLIENT_ID,
+         process.env.GOOGLE_OAUTH_CLIENT_SECRET,
+         process.env.REDIRECT_URI
+     );
+ 
+     console.log('oauthclient Connection successfull')
+ 
+     const scopes = [
+         'https://www.googleapis.com/auth/drive.metadata.readonly'
+     ];
+ 
+     console.log('Scope set done')
+ 
+     const state = crypto.randomBytes(32).toString('hex');
+ 
+     // Generate a url that asks permissions for the Drive activity scope
+ 
+     const authorizationUrl = oauth2Client.generateAuthUrl({
+     // 'online' (default) or 'offline' (gets refresh_token)
+     access_type: 'offline',
+     scope: scopes,
+     include_granted_scopes: true,
+     // Include the state parameter to reduce the risk of CSRF attacks.
+     state: state
+    });
+ 
+    if(!authorizationUrl){
+     throw new Apierror(500,'Failed to Get AUthorization Url')
+    }
+ 
+    // console.log("Got AUthorizational url",authorizationUrl)
+ 
+    return res.status(200).json(
+     new Apiresponse(200,"got he Authorization Token from google oauth api",authorizationUrl)
+    )
+   } catch (error) {
+    console.log("An error Occurred while getting Authorization token",error)
    }
-
-   console.log("Got AUthorizational url",authorizationUrl)
 }
 
 export { 
